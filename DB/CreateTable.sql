b386d0c5fe5f38237aeae470f28ba2ec9a74b2bd
S-Kelian <80928195+S-Kelian@users.noreply.github.com>
2021-12-03 00:28:19 +0100

    Update Create

--- DB/CreateTable.sql
+++ DB/CreateTable.sql
@@ -6,67 +6,59 @@ DROP TABLE IF EXISTS Decorations;
 DROP TABLE IF EXISTS ActionSauvetages;
 
 CREATE TABLE Bateaux(
-    idBateau NUMERIC(4) NOT NULL AUTO_INCREMENT START WITH 1,
     nomBateau VARCHAR(30),
     lienImageBateau VARCHAR(40),
-    descriptionBateau VARCHAR(1000),
-    CONSTRAINT pk_bateau PRIMARY KEY (idBateau),
-    CONSTRAINT uk_bateau_nombateau UNIQUE (nomBateau)
+    descriptionBateau VARCHAR(10000),
+    CONSTRAINT pk_bateau PRIMARY KEY (nomBateau)
 );
 
 CREATE TABLE Sauveteurs(
-    idSauveteur NUMERIC(4) NOT NULL AUTO_INCREMENT START WITH 1,
     nomSauveteur VARCHAR(30),
     imageSauveteur VARCHAR(40),
     anneeNaissance VARCHAR(4),
     anneeMort VARCHAR(4),
-    descriptionSauveteur VARCHAR(1000),
-    etatCivil VARCHAR(1000),
-    donneesgenealogiques VARCHAR(1000),
+    descriptionSauveteur VARCHAR(10000),
+    etatCivil VARCHAR(10000),
+    donneesGenealogiques VARCHAR(10000),
     nbSauve INTEGER(10),
-    Carrierre VARCHAR(1000),    
-    CONSTRAINT pk_sauveteur PRIMARY KEY (idSauveteur) ,
-    CONSTRAINT uk_sauveteur_nomsauveteur UNIQUE (nomSauveteur)  
+    Carriere VARCHAR(10000),    
+    CONSTRAINT pk_sauveteur PRIMARY KEY (nomSauveteur)
 );
 
 CREATE TABLE Decorations(
-    idDecoration NUMERIC(4) NOT NULL AUTO_INCREMENT START WITH 1,
+    idDecoration int AUTO_INCREMENT PRIMARY KEY,
     nomSauveteur VARCHAR(30),
-    dateRemise Date,
+    dateRemise VARCHAR(20),
     nomDecoration VARCHAR(50),
-    CONSTRAINT pk_decorations PRIMARY KEY (idDecoration),
     CONSTRAINT fk_decorations_nomsauveteur FOREIGN KEY (nomSauveteur) REFERENCES Sauveteurs (nomSauveteur)
 );
 
 CREATE TABLE ActionSauvetages(
-    idActionSauvetage NUMERIC(4) NOT NULL AUTO_INCREMENT START WITH 1,
+    idActionSauvetage int AUTO_INCREMENT PRIMARY KEY, 
     nomSauveteur VARCHAR(30),
     annee VARCHAR(4),
-    descriptionAction VARCHAR(1000)
+    descriptionAction VARCHAR(10000),
     CONSTRAINT pk_actionsauvetages PRIMARY KEY (idActionSauvetage),
     CONSTRAINT fk_actionsauvetages_nomsauveteur FOREIGN KEY (nomSauveteur) REFERENCES Sauveteurs (nomSauveteur)
 );
 
 CREATE TABLE Sauvetages(
-    idSauvetage NUMERIC(4) NOT NULL AUTO_INCREMENT START WITH 1,
-    nomSauveteur NUMERIC(30),
     titre VARCHAR(30),
+    nomSauveteur NUMERIC(30),    
     lieuSauvetage VARCHAR(20),
     dateSauvetage DATE,
     nbEquipageSauve INTEGER(3),
     nbSauve INTEGER(3),
     descriptionSauvetage VARCHAR(10000),
     nomBateau VARCHAR(15),
-    CONSTRAINT pk_sauvetages PRIMARY KEY (idSauvetage),
+    CONSTRAINT pk_sauvetages PRIMARY KEY (titre),
     CONSTRAINT fk_sauvetages_nomsauveteur FOREIGN KEY (nomSauveteur) REFERENCES Sauveteurs (nomSauveteur),
     CONSTRAINT fk_sauvetages_nombateau FOREIGN KEY (nomBateau) REFERENCES Bateaux (nomBateau)
 );
 
 CREATE TABLE Sauves(
-    idSauve NUMERIC(4) NOT NULL AUTO_INCREMENT START WITH 1,
     nomSauve VARCHAR(10),
     nomSauvetage VARCHAR(3),
-    CONSTRAINT pk_sauve PRIMARY KEY (idSauve),
-    CONSTRAINT fk_sauve_nomsauvetage FOREIGN KEY (nomSauvetage) REFERENCES Sauveteurs (nomSauvetage),
-    CONSTRAINT uk_bateau_nombateau UNIQUE (nomBateau) 
+    CONSTRAINT pk_sauve PRIMARY KEY (nomSauve),
+    CONSTRAINT fk_sauve_nomsauvetage FOREIGN KEY (nomSauvetage) REFERENCES Sauveteurs (nomSauvetage)
 );
